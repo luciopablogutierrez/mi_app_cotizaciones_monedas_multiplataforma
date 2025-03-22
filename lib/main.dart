b8c801e2fe412a22376dcbb782215ea4aa64f659
Lucio Gutierrez <lucio.gutierrez@gmail.com>
2025-03-22 09:46:58 -0300

    main dart update

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi App',
+      title: 'Mi App de Cotizaciones',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mi App'),
+          title: Text('MiAppCotizaciones.com'),
         ),
         body: Center(
-          child: Text('¡Hola, mundo!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'API de Cotizaciones de Monedas',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              CountryButton(countryName: 'Argentina'),
+              CountryButton(countryName: 'Chile'),
+              CountryButton(countryName: 'Venezuela'),
+              CountryButton(countryName: 'Uruguay'),
+              CountryButton(countryName: 'México'),
+              CountryButton(countryName: 'Bolivia'),
+              CountryButton(countryName: 'Brasil'),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CountryButton extends StatelessWidget {
+  final String countryName;
+
+  CountryButton({required this.countryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(countryName),
       ),
     );
   }
